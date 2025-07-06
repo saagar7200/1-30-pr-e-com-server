13b90465a62d5b2247dcdbd20597325fa1e5dc19
Sagar Bhandari <rsaagar7200@gmail.com>
2025-07-06 14:01:26 +0545

    populate

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { asyncHandler } from "../utils/async-handler.utils";
 import Product from "../models/product.model";
 import Order from "../models/order.model";
+import CustomError from "../middlewares/error-handler.middleware";
 
 export const create = asyncHandler(async (req: Request, res: Response) => {
   const user = req.user._id;
@@ -45,11 +46,72 @@ export const create = asyncHandler(async (req: Request, res: Response) => {
   });
 });
 
-
 // get all orders (only admin)
+export const getAllOrders = asyncHandler(
+  async (req: Request, res: Response) => {
+    const allOrders = await Order.find()
+      .populate("user", "-password")
+      .populate("items.product")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({
+      message: "All orders fetched",
+      status: "success",
+      success: true,
+      data: allOrders,
+    });
+  }
+);
 
 // get all order for user (only user)
+export const getAllByUserId = asyncHandler(
+  async (req: Request, res: Response) => {
+    const user = req.user._id;
 
-// delete order (admin, user)
+    const allOrders = await Order.find({ user })
+      .populate("user", "-password")
+      .populate("items.product")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({
+      message: "All orders fetched",
+      status: "success",
+      success: true,
+      data: allOrders,
+    });
+  }
+);
+
+// delete order (admin)
+export const remove = asyncHandler(async (req: Request, res: Response) => {
+  const orderId = req.params.id;
+
+  const deletedOrder = await Order.findByIdAndDelete(orderId);
+
+  if (!deletedOrder) {
+    throw new CustomError("Order not found", 400);
+  }
+  res.status(200).json({
+    message: "Order deleted successfully.",
+    status: "success",
+    success: true,
+    data: deletedOrder,
+  });
+});
 
 // get order by id (user , admin )
+
+export const getById = asyncHandler(async (req: Request, res: Response) => {
+  const orderId = req.params.id;
+
+  const order = await Order.findById(orderId)
+    .populate("user", "-password")
+    .populate("items.product");
+
+  res.status(200).json({
+    message: "Order fetched",
+    status: "success",
+    success: true,
+    data: order,
+  });
+});
